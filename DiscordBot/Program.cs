c42b2b30ffb639c0f7aac7b2f09f38bd33468d51
maldron0309 <bagjinhyeong640@gmail.com>
2024-05-22 09:48:40 +0900

    Add project files.

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using DiscordBot.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System.Threading.Tasks;
+
+
+namespace DiscordBot
+{
+    internal class Program
+    {
+        private static DiscordClient client { get; set; }
+        private static CommandsNextExtension commands { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            client = new DiscordClient(discordConfig);
+
+            client.Ready += Client_Ready;
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
